3884e7bb73d06f32e2b324a256097670688d2481
lahemikk <milahe@taltech.ee>
2025-03-03 20:30:52 +0200

    Week 10 push

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    let url = 'https://api.themoviedb.org/3/movie/603?api_key=115f690319e7033ad83716129630f553';
+    axios.get(url)
+    .then(response => {
+        let data = response.data;
+        let releaseDate = new Date(data.release_date).getFullYear();
+
+        let genresToDisplay = '';
+        data.genres.forEach(genre => {
+            genresToDisplay = genresToDisplay + `${genre.name}, `;
+        });
+        let genresUpdated = genresToDisplay.slice(0, -2) + '.';
+
+        let posterUrl = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${data.poster_path}`;
+
+        let currentYear = new Date().getFullYear();
+
+
+        res.render('index', {
+            dataToRender: data,
+            year: currentYear,
+            releaseYear: releaseDate,
+            genres: genresUpdated,
+            poster: posterUrl
+        });
+    });
+
+    
+});
+
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('Server is running');
+});
